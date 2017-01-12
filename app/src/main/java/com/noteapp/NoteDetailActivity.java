c34d7c8f4cb81d645dea5350dec524f041e5a3b2
Jose Artacho <jad1996@gmail.com>
2017-01-12 11:18:20 +0100

    Create detail note activity

---
+++ app/src/main/java/com/noteapp/NoteDetailActivity.java
@@ -0,0 +1,15 @@
+package com.noteapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.noteapp.noteapp.R;
+
+public class NoteDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_detail);
+    }
+}
